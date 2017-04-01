478da24e18486bf54862a20cfe4e51b5e717bfa1
Jordan Assayah <jordan.assayah@apptitude.ch>
2017-04-01 09:37:38 +0200

    Fix tests

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  if (swaggerExpress.runner.swagger.paths['/api/v1/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/hello?name=Scott');
   }
 });
