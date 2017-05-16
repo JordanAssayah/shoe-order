95c75b837438d05b87d622b3925a8957af4194e2
Jordan Assayah <jordanassayah@gmail.com>
2017-05-16 16:48:57 +0200

    #13-CTS Add register form

---
+++ frontend/src/pages/Auth/Register.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <div id="register">
+    <h1 class="title is-1 has-text-centered">Register</h1>
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <div class="columns is-multiline">
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="text" placeholder="First name">
+            </p>
+          </div>
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="text" placeholder="Last name">
+            </p>
+          </div>
+
+
+
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="text" placeholder="Username">
+            </p>
+          </div>
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="password" placeholder="password">
+            </p>
+          </div>
+
+
+
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="text" placeholder="Email">
+            </p>
+          </div>
+          <div class="column is-6">
+            <p class="control">
+              <input class="input" type="password" placeholder="Password">
+            </p>
+          </div>
+
+
+
+          <div class="column is-6">
+            <datepicker placeholder="Birthday" :config="{ dateFormat: 'Y-m-d', static: true }"></datepicker>
+          </div>
+          <div class="column is-6">
+            <p>User must have at least 16 years old</p>
+          </div>
+
+          <div class="column is-8 is-offset-2">
+            <button type="button" class="button is-success is-fullwidth">Create account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Datepicker from 'vue-bulma-datepicker'
+
+export default {
+  data () {
+    return {
+      date: ''
+    }
+  },
+  components: {
+    Datepicker
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import Router       from 'vue-router'
 import Home         from '@/pages/Home/Home'
 import ShoesManager from '@/pages/Admin/ShoesManager'
 import Articles     from '@/pages/Articles/Articles'
+import Register     from '@/pages/Auth/Register'
 
 Vue.use(Router)
 
@@ -11,6 +12,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/shoes-manager', name: 'ShoesManager', component: ShoesManager },
-    { path: '/list-of-shoes', name: 'ShoesList', component: Articles }
+    { path: '/list-of-shoes', name: 'ShoesList', component: Articles },
+    { path: '/register', name: 'Register', component: Register }
   ]
 })
