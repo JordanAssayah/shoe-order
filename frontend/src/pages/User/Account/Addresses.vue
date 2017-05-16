d454e086880ad4d4a276c7cac787f5740281977c
Jordan Assayah <jordanassayah@gmail.com>
2017-05-16 12:11:13 +0200

    #11-CTS Add views for profile, addresses and orders

---
+++ frontend/src/pages/User/Account/Addresses.vue
@@ -0,0 +1,65 @@
+<template lang="html">
+  <div id="user-addresses">
+    <div class="columns is-multiline">
+      <div class="column is-6-desktop is-offset-3-desktop is-8-tablet is-offset-2-tablet">
+
+
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              <span class="title is-4">Home</span>
+            </p>
+            <a class="card-header-icon">
+              <button class="button is-small" @click="editAddress">EDIT</button>
+            </a>
+            <a class="card-header-icon">
+              <button class="delete" @click="deleteAddress"></button>
+            </a>
+          </header>
+          <div class="card-content">
+            <div class="columns">
+              <div class="column is-2">
+                <p>default</p>
+              </div>
+              <div class="column">
+                Billy Bob, Rue central 27 bis 1009 Lausanne, +41 79 775 99 83
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="column is-2-desktop is-offset-3-desktop is-offset-2-tablet is-2-tablet">
+        <button type="button" class="button is-primary" @click="addAddress">Add an address</button>
+      </div>
+      <div class="column is-4-desktop is-6-tablet">
+        <div class="box">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CTSUserAddresses',
+  methods: {
+    addAddress () {
+      console.log('addAddress')
+    },
+    editAddress () {
+      console.log('editAddress')
+    },
+    deleteAddress () {
+      console.log('deleteAddress')
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  #user-addresses
+    margin-top: 30px
+</style>
---
+++ frontend/src/pages/User/Account/BasicInfo.vue
@@ -0,0 +1,82 @@
+<template lang="html">
+  <div id="user-profile">
+    <div class="columns">
+      <div class="column is-4-desktop is-offset-4-desktop">
+        <div class="columns is-multiline">
+          <div class="column is-4">
+            <div class="field-label is-normal is-pulled-left">
+              <label class="label">First name</label>
+            </div>
+          </div>
+          <div class="column is-8">
+            <p class="control">
+              <input class="input" type="text" placeholder="Text input">
+            </p>
+          </div>
+
+          <div class="column is-4">
+            <div class="field-label is-normal is-pulled-left">
+              <label class="label">last name</label>
+            </div>
+          </div>
+          <div class="column is-8">
+            <p class="control">
+              <input class="input" type="text" placeholder="Text input">
+            </p>
+          </div>
+
+          <div class="column is-4">
+            <div class="field-label is-normal is-pulled-left">
+              <label class="label">Email</label>
+            </div>
+          </div>
+          <div class="column is-8">
+            <p>xsdfgdsfg@test.com</p>
+          </div>
+
+          <div class="column is-4">
+            <div class="field-label is-normal is-pulled-left">
+              <label class="label">Phone</label>
+            </div>
+          </div>
+          <div class="column is-8">
+            <p class="control">
+              <input class="input" type="text">
+            </p>
+          </div>
+
+          <div class="column is-4">
+            <div class="field-label is-normal is-pulled-left">
+              <label class="label">Birthdate</label>
+            </div>
+          </div>
+          <div class="column is-8">
+            <p>29 Août 1997</p>
+          </div>
+
+          <div class="column is-10 is-offset-1">
+            <button type="button" class="button is-fullwidth" @click="saveModifications">
+              Save modifications
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CTSProfile',
+  methods: {
+    saveModifications () {
+      console.log('saveModifications')
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+#user-profile
+  margin-top: 30px
+</style>
