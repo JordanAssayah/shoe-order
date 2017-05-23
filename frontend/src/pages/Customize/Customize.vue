063379c44394cb886a4af162d81c8662ae4abf3a
Jordan Assayah <jordanassayah@gmail.com>
2017-05-23 16:48:39 +0200

    #20 Add the customize route, add the base page

---
+++ frontend/src/pages/Customize/Customize.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div id="customize">
+    <h1 class="title is-1 has-text-centered">Customs</h1>
+    <!-- <h3 v-if="isLoadingModels" class="title is-3 has-text-centered">Models are loading...</h3> -->
+    <div class="columns is-multiline">
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  mapGetters,
+  mapActions
+} from 'vuex'
+
+export default {
+  name: 'CTSCustomize',
+  computed: mapGetters([
+
+  ]),
+  methods: mapActions([
+
+  ])
+}
+</script>
+
+<style lang="sass">
+</style>
